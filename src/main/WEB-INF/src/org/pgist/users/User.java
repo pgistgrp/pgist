50418ee45b381a2ef4ae3b9c3de8cd40e2154975
kenny <>
2005-06-21 18:03:01 +0000

    initial.

---
+++ src/main/WEB-INF/src/org/pgist/users/User.java
@@ -0,0 +1,44 @@
+package org.pgist.users;
+
+
+/**
+ * PGIST User class.
+ * @hibernate.class table="pgist_user"
+ */
+public class User {
+    
+    
+    private Long id;
+    private String loginname;
+    
+    
+    /**
+     * @return
+     * @hibernate.id generator-class="native"
+     */
+    public Long getId() {
+        return id;
+    }
+    
+    
+    public void setId(Long it) {
+        this.id = id;
+    }
+    
+    
+    /**
+     * @return
+     * @hibernate.property
+     */
+    public String getLoginname() {
+        return loginname;
+    }
+    
+    
+    public void setLoginname(String loginname) {
+        this.loginname = loginname;
+    }
+    
+    
+}
+
