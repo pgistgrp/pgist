552e6007012adce295044c3f6adcc115ecb4a9c2
kenny <>
2005-06-23 11:19:12 +0000

    initial.

---
+++ src/main/WEB-INF/src/org/pgist/tests/SystemInit.java
@@ -0,0 +1,134 @@
+package org.pgist.tests;
+
+import java.io.File;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.DirectoryScanner;
+import org.apache.tools.ant.Project;
+import org.apache.tools.ant.taskdefs.MatchingTask;
+import org.apache.tools.ant.types.FileSet;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.util.ArrayHelper;
+import org.pgist.users.User;
+
+
+/**
+ * Init the PGIST system
+ * @author kenny
+ *
+ */
+public class SystemInit extends MatchingTask {
+    
+    
+    private List fileSets = new LinkedList();
+    private String configurationFile = null;
+
+    
+    public void addFileset(FileSet set) {
+        fileSets.add(set);
+    }
+
+    
+    /**
+     * Set a properties file
+     * @param propertiesFile the properties file name
+     */
+    public void setProperties(File propertiesFile) {
+        if ( !propertiesFile.exists() ) {
+            throw new BuildException("Properties file: " + propertiesFile + " does not exist.");
+        }
+
+        log("Using properties file " + propertiesFile, Project.MSG_DEBUG);
+    }
+    
+
+    /**
+     * Set a <literal>.cfg.xml</literal> file, which will be
+     * loaded as a resource, from the classpath
+     * @param configurationFile the path to the resource
+     */
+    public void setConfig(String configurationFile) {
+        this.configurationFile = configurationFile;
+    }
+
+
+    /**
+     * Execute the task
+     */
+    public void execute() throws BuildException {
+        try {
+            Configuration cfg = getConfiguration();
+            SessionFactory sessionFactory = cfg.buildSessionFactory();
+            
+            //initialization begins here
+            
+            /* create administrator user */
+            try {
+                Session session = sessionFactory.openSession();
+                Transaction transaction = session.beginTransaction();
+                User admin = new User();
+                admin.setLoginname("admin");
+                admin.setOriginPassword("admin");
+                admin.setEnabled(true);
+                admin.setDeleted(false);
+                session.save(admin);
+                transaction.commit();
+                System.out.println("---- successfully inserte a user: admin");
+            } catch(Exception ex) {
+                System.out.println("---- failed to inserte a user: admin");
+            }
+            
+            //initialization ends here
+            
+        } catch (Exception e) {
+            throw new BuildException(e);
+        }
+    }
+
+    private String[] getFiles() {
+
+        List files = new LinkedList();
+        for ( Iterator i = fileSets.iterator(); i.hasNext(); ) {
+
+            FileSet fs = (FileSet) i.next();
+            DirectoryScanner ds = fs.getDirectoryScanner( getProject() );
+
+            String[] dsFiles = ds.getIncludedFiles();
+            for (int j = 0; j < dsFiles.length; j++) {
+                File f = new File(dsFiles[j]);
+                if ( !f.isFile() ) {
+                    f = new File( ds.getBasedir(), dsFiles[j] );
+                }
+
+                files.add( f.getAbsolutePath() );
+            }
+        }
+
+        return ArrayHelper.toStringArray(files);
+    }
+
+    private Configuration getConfiguration() throws Exception {
+        Configuration cfg = new Configuration();
+        if (configurationFile != null) cfg.configure( new File(configurationFile) );
+
+        String[] files = getFiles();
+        for (int i = 0; i < files.length; i++) {
+            String filename = files[i];
+            if ( filename.endsWith(".jar") ) {
+                cfg.addJar( new File(filename) );
+            }
+            else {
+                cfg.addFile(filename);
+            }
+        }
+        return cfg;
+    }
+
+
+}
