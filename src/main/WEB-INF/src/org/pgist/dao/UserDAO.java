bdf21fd583e3a9a89b9d4b0581899ce39b719697
kenny <>
2005-06-23 09:17:47 +0000

    initial.

---
+++ src/main/WEB-INF/src/org/pgist/dao/UserDAO.java
@@ -0,0 +1,54 @@
+package org.pgist.dao;
+
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.pgist.users.User;
+import org.pgist.util.HibernateUtil;
+
+
+/**
+ * DAO for query related to Users
+ * @author kenny
+ *
+ */
+public class UserDAO {
+
+    
+    /**
+     * Get User object by the given name and query conditions
+     * @param loginname
+     * @param enabled
+     * @param deleted
+     * @return
+     */
+    public static User getUserByName(String loginname, boolean enabled, boolean deleted) {
+        User user = null;
+        
+        try {
+            Session session = HibernateUtil.getSession();
+            HibernateUtil.begin();
+            
+            Query query = session.createQuery("from User where loginname=:loginname and enabled=:enabled and deleted=:deleted");
+            query.setString("loginname", loginname);
+            query.setBoolean("enabled", true);
+            query.setBoolean("deleted", false);
+            List list = query.list();
+            if (list.size()==0) return user;
+            
+            user = (User) list.get(0);
+            
+            HibernateUtil.commit();
+        } catch (Exception e) {
+            try {
+                HibernateUtil.rollback();
+            } catch(Exception ex) {
+            }
+        }
+        
+        return user;
+    }
+    
+    
+}
