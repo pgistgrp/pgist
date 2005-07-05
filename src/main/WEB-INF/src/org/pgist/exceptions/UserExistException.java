75a759e55cbc8f338767663a998ef422ec7824c1
kenny <>
2005-07-05 10:43:04 +0000

    initial.

---
+++ src/main/WEB-INF/src/org/pgist/exceptions/UserExistException.java
@@ -0,0 +1,14 @@
+package org.pgist.exceptions;
+
+public class UserExistException extends Exception {
+
+    
+    private static final long serialVersionUID = 1L;
+    
+
+    public UserExistException(String message) {
+        super(message);
+    }
+    
+    
+}
