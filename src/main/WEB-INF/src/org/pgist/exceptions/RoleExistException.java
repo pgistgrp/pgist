8137cc829f4c0513b96ad9668f6153a6bac4648e
kenny <>
2005-07-08 11:37:27 +0000

    initial.

---
+++ src/main/WEB-INF/src/org/pgist/exceptions/RoleExistException.java
@@ -0,0 +1,14 @@
+package org.pgist.exceptions;
+
+public class RoleExistException extends Exception {
+    
+    
+    private static final long serialVersionUID = 1L;
+    
+
+    public RoleExistException(String message) {
+        super(message);
+    }
+    
+
+}
