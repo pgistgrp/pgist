f048960de660cdce2807a8431f6ac7770a42b09e
kenny <>
2005-08-15 16:21:24 +0000

    initial.

---
+++ src/main/WEB-INF/src/org/pgist/backing/GlossaryBean.java
@@ -0,0 +1,12 @@
+package org.pgist.backing;
+
+
+/**
+ * Backing bean for processing glossary management
+ * 
+ * @author kenny
+ *
+ */
+public class GlossaryBean {
+
+}//class GlossaryBean
---
+++ src/main/WEB-INF/src/org/pgist/dao/GlossaryDAO.java
@@ -0,0 +1,14 @@
+package org.pgist.dao;
+
+
+/**
+ * DAO for query related to Glossary
+ * 
+ * @author kenny
+ *
+ */
+public class GlossaryDAO extends BaseDAO {
+
+    
+    
+}//class GlossaryDAO
